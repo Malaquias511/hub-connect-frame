b9bc1722070c1331c82ac9cfedd7daf1b3a1227e
Stelio Jaime  Nharimue <X211338@millenniumbim.co.mz>
2026-04-01 09:48:09 +0200

    Apptsx intra to Inta

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ import Leave from "./pages/Leave";
 import Evaluations from "./pages/Evaluations";
 import HRPortal from "./pages/HRPortal";
 import Intranet from "./pages/Intranet";
-import Intra from "./pages/intra";
+import Intra from "./pages/Intra";
 import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
 import PortalDocumentacao from "./pages/doc";
